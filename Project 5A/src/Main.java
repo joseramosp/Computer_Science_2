8477bc3d2dfa2b55f8361ffd86d69669575a0faf
Vladimir Ventura <vladventura@outlook.es>
2019-12-07 22:30:08 -0500

    Just as a testament that the to/from file works, the tree is built when the program starts

--- Project 5A/src/Main.java
+++ Project 5A/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args){
         BSTree tree = new BSTree();
+        tree.generateQuestions();
+        tree.toFile();
         TwentyQuestions tq = new TwentyQuestions(tree);
         tq.setup();
         tq.startGame();
