a92f0baa13bda8faa1e77996d2007b68b0af9b28
Vladimir Ventura <vladventura@outlook.es>
2019-12-07 22:37:30 -0500

    I noticed that rebuilding the whole file over and over again just to showcase the to/from file methods work is not really efficient

--- Project 5A/src/Main.java
+++ Project 5A/src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args){
         BSTree tree = new BSTree();
-        tree.generateQuestions();
-        tree.toFile();
         TwentyQuestions tq = new TwentyQuestions(tree);
         tq.setup();
         tq.startGame();
